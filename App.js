17e46646dc5bbd0aa13bf7f36997437c7e2c48db
Gideon Crawley <gscrawley@gmail.com>
2021-07-20 18:29:31 -0700

    created Welcome component

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import GameBoard from './components/GameBoard'
-import { Provider } from 'react-redux'
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import Welcome from './Welcome';
+import GameBoard from './GameBoard';
+import { getFocusedRouteNameFromRoute, NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Provider } from 'react-redux';
 import store from './redux/store'
+
+import { ionIcon } from  'react-native-vector-icons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+    <Tab.Navigator 
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused 
+              ? 'ios-home'
+              : 'ios-home-outline';
+          } else if (route.name === 'Game') {
+            iconName = focused 
+            ? 'hammer-outline' : 'hammer'
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+>
     <Provider store={store}>
-      <GameBoard/>
+
     </Provider>
+    
+ </Tab.Navigator>
+ </NavigationContainer>
   );
 }
 
---
+++ components/Welcome.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react' 
+import { StyleSheet, Text, View, ImageBackground  } from 'react-native'
+import GameBoard from './GameBoard'
+
+export default function Welcome() {
+	return (
+		<ImageBackground 
+			style={styles.container}
+      source={require('../assets/background.png')}
+      >
+    <Text style={styles.header}>Welcome </Text>
+    <Text style={styles.header}> To </Text>
+    <Text style={styles.header}> Whack-A-Mole! </Text>
+      </ImageBackground>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'lightblue',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    header: {
+        fontWeight: 'bold',
+        marginBottom: 10,
+        marginTop: 100,
+        color: 'purple'
+    }
+
+  });
